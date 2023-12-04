628c634bfb32b7b87542797e8984c3fc5d905df7
LeoGidev <leogimenez.dev@gmail.com>
2023-12-04 13:59:19 -0300

    se realiza ventana grafica

--- calculadormascara.py
+++ calculadormascara.py
@@ -1,43 +1,64 @@
-import tkinter
-from tkinter import*
-from tkinter import ttk
+from tkinter import *
+import tkinter as tk
 
 
 
 
-def calcular_mascara_red(x):
-    # Calcula los octetos completos y los bits adicionales
-    octetos_completos = x // 8
-    bits_adicionales = x % 8
 
-    # Construye la máscara de red
-    mascara = [255] * octetos_completos
+def calcular_mascara_red():
+    x=int(textExample.get(1.0, tk.END+"-1c"))
+    if x < 33  and x > -1:
 
-    if bits_adicionales > 0:
-        valor_bits_adicionales = 2 ** (8 - bits_adicionales) - 1
-        mascara.append(valor_bits_adicionales)
+        # Calcula los octetos completos y los bits adicionales
+        octetos_completos = x // 8
+        bits_adicionales = x % 8
+
+        # Construye la máscara de red
+        mascara = [255] * octetos_completos
+
+        if bits_adicionales > 0:
+            valor_bits_adicionales = 2 ** (8 - bits_adicionales) - 1
+            mascara.append(valor_bits_adicionales)
     
-    # Completa la máscara de red con ceros
-    while len(mascara) < 4:
-        mascara.append(0)
+        # Completa la máscara de red con ceros
+        while len(mascara) < 4:
+            mascara.append(0)
     
-    return ".".join(map(str, mascara))
+        respuesta.delete(0, END)
+
+        respuesta.insert(10,".".join(map(str, mascara)))
+        #return ".".join(map(str, mascara))
+    else:
+        mascara = "el máximo valor de máscara es 32 y el minimo es 0"
+        respuesta.delete(0, END)
+        respuesta.insert(10,mascara)
+    
+
+    
 
 
 #creamos la ventana del programa
-ventana = tkinter.Tk()
-ventana.geometry('400x400')
-ventana.title('Calculador de máscara de red')
-#imagen = PhotoImage(file="logo.png")
-Label(ventana,  bd=0).pack()
+root = tk.Tk()
+root.geometry("400x240")
+root.title("Calcular máscara")
 
 
-# Solicita al usuario ingresar el valor "/x"
-valor_x = int(input("Ingrese el valor '/x' para la máscara de red: "))
 
-# Calcula y muestra la máscara de red correspondiente
-mascara_red = calcular_mascara_red(valor_x)
-print("La máscara de red correspondiente es:", mascara_red)
+#creamos la variable entrada y la poscionamos
+
+textExample=tk.Text(root, height=1, width=15)
+textExample.pack()
+btnRead=tk.Button(root, height=1, width=10, text="Calcular", command=calcular_mascara_red)
+
+
+btnRead.pack()
+
+respuesta = tk.Entry(root)
+respuesta.pack()
+
+
+
+root.mainloop()
 
 
 
