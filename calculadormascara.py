8288a474353d5b983b4ce1ea738258b030d111a7
LeoGidev <leogimenez.dev@gmail.com>
2024-02-18 03:02:51 -0300

    se reestructura app con poo

--- calculadormascara.py
+++ calculadormascara.py
@@ -43,7 +43,7 @@ class calculadoraRED:
 
 
     def mascaraRed(self):
-        x = self.texto.get("1.0", "end")
+        x = self.texto.get("1.0", "end-1c")
         x = x.strip("\n")
         x=int(x)
         print(x)
@@ -53,15 +53,15 @@ class calculadoraRED:
             print('holq')
 
             # Calcula los octetos completos y los bits adicionales
-            #octetos_completos = x // 8
-            #bits_adicionales = x % 8
+            octetos_completos = x // 8
+            bits_adicionales = x % 8
 
             # Construye la máscara de red
-            #mascara = [255] * octetos_completos
+            mascara = [255] * octetos_completos
 
             #if bits_adicionales > 0:
-                #valor_bits_adicionales = 2 ** (8 - bits_adicionales) - 1
-                #mascara.append(valor_bits_adicionales)
+            valor_bits_adicionales = 2 ** (8 - bits_adicionales) - 1
+            mascara.append(valor_bits_adicionales)
         
             #Completa la máscara de red con ceros
             #while len(mascara) < 4:
